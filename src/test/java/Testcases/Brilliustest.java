4985dd0b951aef3d13cf44a143433e3b15ec98f4
Akhiltejachinnam <tejaakhilchinnam@gmail.com>
2024-01-24 10:36:27 +0530

    4th commit

--- src/test/java/Testcases/Brilliustest.java
+++ src/test/java/Testcases/Brilliustest.java
@@ -14,9 +14,73 @@ import Utilities.ReadXLSdata;
 public class Brilliustest extends BaseTest{
 	
 	@Test
+	public static void AdminLoginTest() throws InterruptedException {
+	
+		driver.findElement(By.id(loc.getProperty("username_field"))).sendKeys("Kumar");
+		driver.findElement(By.id(loc.getProperty("password_field"))).sendKeys("Brillius@AI");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath(loc.getProperty("login_next_button"))).click();
+		Thread.sleep(2000);
+		
+		WebElement adminPageElement = driver.findElement(By.xpath(loc.getProperty("AdminPage_Identifier"))); 
+
+	    if (adminPageElement.isDisplayed()) {
+	        System.out.println("Admin page is visible. Login successful!");
+	        
+	    } else {
+	        System.out.println("Admin page is not visible. Login failed!");
+	        
+	    }
+	}
+	
+	@Test
+	public static void InvalidLoginTest() throws InterruptedException {
+		
+		
+		driver.findElement(By.id(loc.getProperty("username_field"))).sendKeys("tyuugf");
+		driver.findElement(By.id(loc.getProperty("password_field"))).sendKeys("123456");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath(loc.getProperty("login_next_button"))).click();
+		Thread.sleep(2000);
+		
+		WebElement invalidmessage = driver.findElement(By.xpath(loc.getProperty("error_flashmessage"))); 
+
+	    if (invalidmessage.isDisplayed()) {
+	        System.out.println("error message is visible.successful!");
+	        
+	    } else {
+	        System.out.println("error message is not visible.failed!");
+	        
+	    }
+	  
+	}
+	
+	@Test
+	public static void EmptyfieldsLoginTest() throws InterruptedException {
+		
+		
+		driver.findElement(By.id(loc.getProperty("username_field"))).sendKeys("");
+		driver.findElement(By.id(loc.getProperty("password_field"))).sendKeys("");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath(loc.getProperty("login_next_button"))).click();
+		Thread.sleep(2000);
+		
+		WebElement invalidmessage = driver.findElement(By.xpath(loc.getProperty("error_flashmessage"))); 
+
+	    if (invalidmessage.isDisplayed()) {
+	        System.out.println("error message is visible.successful!");
+	        
+	    } else {
+	        System.out.println("error message is not visible.failed!");
+	        
+	    }
+	  
+	}
+	
+	@Test
 	public static void UserLoginTest() throws InterruptedException {
 		
-		System.out.println("Clicked successfully");
+		
 		driver.findElement(By.id(loc.getProperty("username_field"))).sendKeys("akhilbrillius");
 		driver.findElement(By.id(loc.getProperty("password_field"))).sendKeys("Amma@143");
 		Thread.sleep(2000);
@@ -32,74 +96,41 @@ public class Brilliustest extends BaseTest{
 	        System.out.println("User page is not visible. Login failed!");
 	        
 	    }
-	    driver.findElement(By.xpath(loc.getProperty("Audio_Session"))).click();
-	    driver.findElement(By.xpath(loc.getProperty("Practice_Session"))).click();
-	    driver.findElement(By.xpath(loc.getProperty("Start_Session"))).click();
-	    	//Thread.sleep(2000);
-	    driver.findElement(By.xpath(loc.getProperty("Get_Earphone"))).click();
-
-	    	try {
-	            while (true) {
-	            	
-	            	// Click on the "Start" button
-	                WebElement startButton = driver.findElement(By.xpath(loc.getProperty("Start")));
-	                startButton.click();
-	                Thread.sleep(2000);
-	                
-	             // Click on the "Feedback" button
-	                WebElement feedbackButton = driver.findElement(By.xpath(loc.getProperty("Feedback")));
-	                feedbackButton.click();
-	                Thread.sleep(2000);
-	                
-	             // Click on the "Stop" button
-	                WebElement stopButton = driver.findElement(By.xpath(loc.getProperty("Stop")));
-	                stopButton.click();
-	                
-	                // Click on the "Next" button
-	                WebElement nextButton = driver.findElement(By.xpath(loc.getProperty("Next_button")));
-	                nextButton.click();
-	                Thread.sleep(2000);	                
-	                
-	                // Check if the "Finish" button is present
-	                WebElement finishButton = driver.findElement(By.xpath(loc.getProperty("Finish")));
-
-	                // If the "Finish" button is found, break the loop
-	                if (finishButton.isDisplayed()) {
-	                    break;
-	                }
-	            }
-	        } catch (Exception e) {
-	            // Handle exceptions as needed
-	            e.printStackTrace();
-	        }
+	      	
 	}
 	    	@Test
-	    	public boolean Adduser(String fname, String lname, String emailadd, String uname,String pword, String cnumber, String role) throws InterruptedException
+	    	public void Admincreatnewuser() throws InterruptedException
 	    	{
-	    	driver.findElement(By.xpath("Register_button")).click();
-	    	driver.findElement(By.id("first_name")).sendKeys(fname);
-	    	driver.findElement(By.id("lastname")).sendKeys(lname);
-	    	driver.findElement(By.id("email_address")).sendKeys(emailadd);
-	    	driver.findElement(By.id("user_name")).sendKeys(uname);
-	    	driver.findElement(By.id("pwd")).sendKeys(pword);
+	    	driver.findElement(By.id(loc.getProperty("username_field"))).sendKeys("Kumar");
+	    	driver.findElement(By.id(loc.getProperty("password_field"))).sendKeys("Brillius@AI");
+	    	Thread.sleep(2000);
+	    	driver.findElement(By.xpath(loc.getProperty("login_next_button"))).click();
+	    	Thread.sleep(2000);
+	    	driver.findElement(By.xpath(loc.getProperty("user_management"))).click();
+	    	driver.findElement(By.xpath(loc.getProperty("Register_button"))).click();
+	    	driver.findElement(By.id(loc.getProperty("first_name"))).sendKeys("Akhil");
+	    	driver.findElement(By.id(loc.getProperty("lastname"))).sendKeys("tejap");
+	    	driver.findElement(By.id(loc.getProperty("email_address"))).sendKeys("Akhilp@gmail.com");
+	    	driver.findElement(By.id(loc.getProperty("user_name"))).sendKeys("pAkhilteja");
+	    	driver.findElement(By.id(loc.getProperty("pwd"))).sendKeys("Amma@143");
 	    	Thread.sleep(3000);
-	    	driver.findElement(By.xpath("contact_number")).getAttribute(cnumber);
+	    	driver.findElement(By.xpath(loc.getProperty("contact_number"))).getAttribute("9063767656");
 	   	      //driver.findElement(By.id("contactnumber")).sendKeys(cnumber);
 	    	
-	    	Select rolelist = new Select(driver.findElement(By.id("User_type")));
-	    	rolelist.selectByVisibleText(role);
+	    	Select rolelist = new Select(driver.findElement(By.id(loc.getProperty("User_type"))));
+	    	String role = "pAkhilteja";
+			rolelist.selectByVisibleText(role);
 	    	Thread.sleep(2000);
 	    	
-	    	driver.findElement(By.xpath("Registration_submit")).click();
+	    	driver.findElement(By.xpath(loc.getProperty("Registration_submit"))).click();
 	    	Thread.sleep(2000);
 	    	
-	    	driver.findElement(By.xpath("Back_Homepage")).click();
-	    	Thread.sleep(2000);
-			return false;	
+	    	driver.findElement(By.xpath(loc.getProperty("Back_Homepage"))).click();
+	    	Thread.sleep(2000);	
 	    	}
 			public boolean isAdminmoduledisplayed()
 			{
-				if(driver.findElement(By.xpath("Admin_homepage_vissible")).isDisplayed())
+				if(driver.findElement(By.xpath(loc.getProperty("Admin_homepage_vissible"))).isDisplayed())
 						{
 					      return true;
 						}else
@@ -107,8 +138,88 @@ public class Brilliustest extends BaseTest{
 							return false;
 						}
 			}
-	    	
+			
+			@Test
+			public static void UseraudiopracticeTest() throws InterruptedException {
+				
+				
+				driver.findElement(By.id(loc.getProperty("username_field"))).sendKeys("akhilbrillius");
+				driver.findElement(By.id(loc.getProperty("password_field"))).sendKeys("Amma@143");
+				Thread.sleep(2000);
+				driver.findElement(By.xpath(loc.getProperty("login_next_button"))).click();
+				Thread.sleep(2000);
+				driver.findElement(By.xpath(loc.getProperty("Audio_Session"))).click();
+			    driver.findElement(By.xpath(loc.getProperty("Practice_Session"))).click();
+			    driver.findElement(By.xpath(loc.getProperty("Start_Session"))).click();
+			    Thread.sleep(2000);
+			    driver.findElement(By.xpath(loc.getProperty("Get_Earphone"))).click();
+			
 	}
+			
+			@Test
+			public static void UseraudioassessmentTest() throws InterruptedException {
+				
+				System.out.println("Clicked successfully");
+			    driver.findElement(By.id(loc.getProperty("username_field"))).sendKeys("akhilbrillius");
+				driver.findElement(By.id(loc.getProperty("password_field"))).sendKeys("Amma@143");
+				Thread.sleep(2000);
+				driver.findElement(By.xpath(loc.getProperty("login_next_button"))).click();
+				Thread.sleep(2000);
+				driver.findElement(By.xpath(loc.getProperty("Audio_Session"))).click();
+			    driver.findElement(By.xpath(loc.getProperty("Assession"))).click();
+			    driver.findElement(By.xpath(loc.getProperty("Start_Session"))).click();
+			    Thread.sleep(2000);
+			    driver.findElement(By.xpath(loc.getProperty("Get_Earphone"))).click();
+			
+			}
+			
+			@Test
+			public static void UservideopracticeTest() throws InterruptedException {
+				
+				System.out.println("Clicked successfully");
+			    driver.findElement(By.id(loc.getProperty("username_field"))).sendKeys("akhilbrillius");
+				driver.findElement(By.id(loc.getProperty("password_field"))).sendKeys("Amma@143");
+				Thread.sleep(2000);
+				driver.findElement(By.xpath(loc.getProperty("login_next_button"))).click();
+				Thread.sleep(2000);
+				driver.findElement(By.xpath(loc.getProperty("Video_Session"))).click();
+			    driver.findElement(By.xpath(loc.getProperty("Practice_Session"))).click();
+			    driver.findElement(By.xpath(loc.getProperty("Start_Session"))).click();
+			    Thread.sleep(2000);
+			    driver.findElement(By.xpath(loc.getProperty("Get_Earphone"))).click();
+			
+	}
+			
+			@Test
+			public static void UservideoassessmentTest() throws InterruptedException {
+				
+				System.out.println("Clicked successfully");
+			    driver.findElement(By.id(loc.getProperty("username_field"))).sendKeys("akhilbrillius");
+				driver.findElement(By.id(loc.getProperty("password_field"))).sendKeys("Amma@143");
+				Thread.sleep(2000);
+				driver.findElement(By.xpath(loc.getProperty("login_next_button"))).click();
+				Thread.sleep(2000);
+				driver.findElement(By.xpath(loc.getProperty("Video_Session"))).click();
+			    driver.findElement(By.xpath(loc.getProperty("Assession"))).click();
+			    driver.findElement(By.xpath(loc.getProperty("Start_Session"))).click();
+			    Thread.sleep(2000);
+			    driver.findElement(By.xpath(loc.getProperty("Get_Earphone"))).click();
+			
+	}
+			@Test
+			public static void forgetpasswordTest() throws InterruptedException {
+				
+				driver.findElement(By.xpath(loc.getProperty("forgot_password"))).click();
+				Thread.sleep(2000);
+				driver.findElement(By.xpath(loc.getProperty("otp_emailaddress"))).sendKeys("Akhilp@gmail.com");
+				WebElement otpInputField = driver.findElement(By.xpath(loc.getProperty("otp_input_field")));
+				String otpValue = "yourOTP";				
+				otpInputField.sendKeys(otpValue);
+				
+			
+			}
+}
+
 
 
 
